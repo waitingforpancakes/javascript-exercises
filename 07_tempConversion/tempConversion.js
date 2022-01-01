22946a9e7c4b764895b6ee8836fc0e43331cb7b2
Grace <greyse.leung@mail.utoronto.ca>
2022-01-01 01:56:35 -0500

    excercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheitTemp) {
+  let celciusTemp = (fahrenheitTemp - 32) * (5/9);
+  return +celciusTemp.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(celciusTemp) {
+  let fahrenheitTemp = (celciusTemp * (9/5)) + 32;
+  return +fahrenheitTemp.toFixed(1);
 };
 
 // Do not edit below this line
