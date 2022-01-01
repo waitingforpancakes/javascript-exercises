71f364c688e7275d6afad8beb24f3384bd994783
Grace <greyse.leung@mail.utoronto.ca>
2022-01-01 01:46:08 -0500

    excercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0){
+            return year % 400 == 0;
+        } else {
+            return true;
+        }
+    }
+    return false;
 };
 
 // Do not edit below this line
