fee4d697d7068424a2ea5d75c2e956b46aed4667
Grace <greyse.leung@mail.utoronto.ca>
2022-01-01 01:04:48 -0500

    excercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
-
+    let strArray = str.split("");
+    let temp = "";
+    for(const letter of strArray){
+        temp = letter + temp;
+    }
+    return temp;
 };
 
 // Do not edit below this line
