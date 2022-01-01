f8892a3913ed344f3abfa45b74c9bc4604551bb7
Grace <greyse.leung@mail.utoronto.ca>
2022-01-01 02:12:20 -0500

    excercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  let sum = 0;
+  for (const num of numArray){
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  let total = 1;
+  for (const num of numArray){
+    total *= num;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  for(let i = num; i > 0; i--){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
