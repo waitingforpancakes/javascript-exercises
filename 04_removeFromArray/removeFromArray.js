10ed92b1ee3601f317b909ad8a5a633981f183fe
Grace <greyse.leung@mail.utoronto.ca>
2022-01-01 01:28:31 -0500

    excercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
 const removeFromArray = function() {
-
+    let args = Array.from(arguments);
+    let arr = args[0];
+    for(let i = 1; i < args.length; i++){
+        let deleteIndex;
+        for(let j = 0; j < arr.length; j++){
+            if (arr[j] === args[i]){
+                deleteIndex = j;
+            }
+        }
+        if(deleteIndex || deleteIndex == 0){
+            arr.splice(deleteIndex, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
