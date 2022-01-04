b0e54fd84328cbc896e014546b5170dc62e911fe
Grace <greyse.leung@mail.utoronto.ca>
2022-01-04 17:22:50 -0500

    excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (+num <= 0) {
+        return "OOPS";
+    }
 
+    if (+num == 1 || +num == 2) {
+        return 1;
+    } else {
+        return fibonacci(+num - 2) + fibonacci(+num - 1);
+    }
 };
 
 // Do not edit below this line
