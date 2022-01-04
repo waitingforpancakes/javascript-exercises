b5694a868dc6f221b7f677864e7122b58d7278c0
Grace <greyse.leung@mail.utoronto.ca>
2022-01-04 17:15:32 -0500

    excercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Get rid of capitals
+    str = str.toLowerCase();
 
+    // Get rid of spaces and compare
+    let forward = str.split(" ");
+    let forwardConcat = forward.reduce(function(previousWords, word) {
+        let temp = word.split("");
+        let forwardWord = temp.reduce(function(previousLetters, letter) {
+            if (!letter.match(/^[.,:!?]/)) {
+                return previousLetters + letter;
+            } else {
+                return previousLetters;
+            }
+        }, "");
+        return previousWords + forwardWord;
+    }, "");
+    let backwardConcat = forwardConcat.split("").reduce(function(previousLetters, letter) {
+        return letter + previousLetters;       
+    }, "");
+
+    return forwardConcat == backwardConcat;
 };
 
 // Do not edit below this line
